2fc316a6572bdc1e9881c8530aa43b343b72b352
Mateusz Waloszek <120412764@umail.ucc.ie>
2022-12-04 12:54:41 +0100

    added testing for green and blue values

--- src/test/java/org/example/ColourTest.java
+++ src/test/java/org/example/ColourTest.java
@@ -16,14 +16,51 @@ class ColourTest {
     public void Test_red_correct_values() {
 
         Throwable exception1 = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Colour colour = new Colour(2.0f, 1.0f, 1.0f);
+            Colour colour1 = new Colour(2.0f, 1.0f, 1.0f);
         });
         Assertions.assertEquals("Red float value must be above 0 and below 1", exception1.getMessage());
 
         Throwable exception2 = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Colour colour = new Colour(-1f, 1.0f, 1.0f);
+            Colour colour2 = new Colour(-1.0f, 1.0f, 1.0f);
         });
         Assertions.assertEquals("Red float value must be above 0 and below 1", exception2.getMessage());
     }
+
+
+    @Test
+    public void Test_green_correct_values() {
+
+        Throwable exception1 = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Colour colour1 = new Colour(1.0f, 2.0f, 1.0f);
+        });
+        Assertions.assertEquals("Green float value must be above 0 and below 1", exception1.getMessage());
+
+        Throwable exception2 = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Colour colour2 = new Colour(1.0f, -1.0f, 1.0f);
+        });
+        Assertions.assertEquals("Green float value must be above 0 and below 1", exception2.getMessage());
+    }
+
+    @Test
+    public void Test_blue_correct_values() {
+
+        Throwable exception1 = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Colour colour1 = new Colour(1.0f, 1.0f, 1.2f);
+        });
+        Assertions.assertEquals("Blue float value must be above 0 and below 1", exception1.getMessage());
+
+        Throwable exception2 = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Colour colour2 = new Colour(1.0f, 1.0f, -22.0f);
+        });
+        Assertions.assertEquals("Blue float value must be above 0 and below 1", exception2.getMessage());
+    }
+
+
+
+
+
+
+
+
 
 }
